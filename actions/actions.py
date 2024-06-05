d418d3144677823993c1c877229793fea9f4f402
Jonh Erik <erik.lima60@aluno.ifce.edu.br>
2024-06-05 15:19:15 -0300

    added cities.json

--- actions/actions.py
+++ actions/actions.py
@@ -1,9 +1,8 @@
-from typing import Any, Text, Dict, List
+import json
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-
-# importação para SlotSet 
 from rasa_sdk.events import SlotSet
+from typing import Any, Text, Dict, List
 
 class ActionSaveCity(Action):
 
@@ -18,10 +17,36 @@ class ActionSaveCity(Action):
         city_entity = next(tracker.get_latest_entity_values("cidade"), None)
         
         if city_entity:
-            dispatcher.utter_message(text=f"você quer cinema de {city_entity}, correto?")
+            dispatcher.utter_message(text=f"Você quer cinema de {city_entity}, correto?")
             # Salva a cidade em uma variável (slot)
             return [SlotSet("cidade", city_entity)]
         else:
             dispatcher.utter_message(text="Desculpe, não consegui identificar a cidade mencionada.")
             return []
 
+class ActionProcurarCidade(Action):
+
+    def name(self) -> Text:
+        return "action_procurar_cidade"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        city = tracker.get_slot('cidade')
+        
+        if not city:
+            dispatcher.utter_message(text="Por favor, mencione uma cidade.")
+            return []
+
+        with open('data/cities.json', 'r', encoding='utf-8') as f:
+            cities_data = json.load(f)
+        
+        cities = [entry['city'].lower() for entry in cities_data]
+
+        if city.lower() in cities:
+            dispatcher.utter_message(text=f"A cidade {city} foi encontrada.")
+        else:
+            dispatcher.utter_message(text="Desculpe, não reconhecemos essa cidade. Você pode tentar outra cidade?")
+        
+        return []
