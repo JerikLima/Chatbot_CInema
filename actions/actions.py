a20850a5e123f0741592e3d0e9ed01c0ac14d711
Jonh Erik <erik.lima60@aluno.ifce.edu.br>
2024-06-07 19:42:56 +0000

    Merged PR 2796: Fix/finished recognize cities in json
    Fix/finished recognize cities in json

--- actions/actions.py
+++ actions/actions.py
@@ -1,29 +1,11 @@
 import json
+import unidecode
 from cinemapercity import Cinema
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
 from typing import Any, Text, Dict, List
 
-class ActionSaveCity(Action):
-
-    def name(self) -> Text:
-        return "action_save_city"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        # Captura a entidade cidade
-        city_entity = next(tracker.get_latest_entity_values("cidade"), None)
-        
-        if city_entity:
-            dispatcher.utter_message(text=f"Você quer cinema de {city_entity}, correto?")
-            # Salva a cidade em uma variável (slot)
-            return [SlotSet("cidade", city_entity)]
-        else:
-            dispatcher.utter_message(text="Desculpe, não consegui identificar a cidade mencionada.")
-            return []
 
 class ActionProcurarCidade(Action):
 
@@ -42,10 +24,8 @@ class ActionProcurarCidade(Action):
 
         with open('data/cities.json', 'r', encoding='utf-8') as f:
             cities_data = json.load(f)
-        
-        cities = [entry['city'].lower() for entry in cities_data]
-
-        if city.lower() in cities:
+        cities = [unidecode.unidecode(entry['city'].lower()) for entry in cities_data]
+        if unidecode.unidecode(city).lower() in cities:
             cinemas = Cinema.obter_nome_cinema_por_cidade(city)
             dispatcher.utter_message(text=f"Os cinemas em {city} são:\n" + '\n'.join(cinemas))
         else:
